3417cd8af5fce94a5738579bd922853c5fe0d8a5
hellomyzn <em711th@icloud.com>
2025-04-05 15:52:43 +0900

    update: for build

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: { unoptimized: true },
+  basePath: "/hellomyzn.github.io",
+  assetPrefix: "/hellomyzn.github.io",
 };
 
 export default nextConfig;
