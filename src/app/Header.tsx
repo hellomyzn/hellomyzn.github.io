1eecb745c3eb6e399cefade38c8aeaf505585203
hellomyzn <em711th@icloud.com>
2025-04-06 18:47:46 +0900

    add: about page

---
+++ src/app/Header.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <header className="fixed top-0 z-50 bg-white left-0 sm:left-auto sm:right-0 border-b border-r sm:border-l sm:border-r-0">
+      <nav className="flex">
+        {/* Home */}
+        <div className="p-3 border-r">
+          <Link
+            href="/"
+            className={`px-2 py-1 w-20 text-center ${
+              pathname === "/" ? "highlight" : ""
+            }`}
+          >
+            Home
+          </Link>
+        </div>
+        {/* Posts */}
+        <div className="p-3 border-r">
+          <Link
+            href="/posts"
+            className={`px-2 py-1 w-20 text-center ${
+              pathname === "/posts" ? "highlight" : ""
+            }`}
+          >
+            Posts
+          </Link>
+        </div>
+        {/* About */}
+        <div className="p-3">
+          <Link
+            href="/about"
+            className={`px-2 py-1 w-20 text-center ${
+              pathname === "/about" ? "highlight" : ""
+            }`}
+          >
+            About
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,120 @@
 import React from "react";
+import { SiQiita } from "react-icons/si";
+
+import {
+  FaGithub,
+  FaInstagram,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter,
+} from "react-icons/fa";
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <div className="flex sm:h-screen flex-col-reverse sm:flex-row">
+      <div className="sm:flex-1 border-t sm:border-t-0">
+        <div className="grid w-full h-full grid-cols-2 grid-rows-[270px_270px] sm:grid-rows-2">
+          <div className="border-r overflow-hidden flex items-end">
+            <img
+              src="/images/enjoy.jpg"
+              alt="hero image"
+              className="w-full object-cover"
+              width="700"
+              height="700"
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
+          <div className="sm:border-r overflow-hidden flex items-center justify-center">
+            <h3 className="sm:text-2xl w-9/12 uppercase"></h3>
+          </div>
+          <div className="border-r border-t overflow-hidden flex items-center px-8">
+            <h3 className="sm:text-2xl"></h3>
+          </div>
+          <div className="sm:border-r border-t overflow-hidden flex">
+            <img
+              src="/images/me2.jpg"
+              alt="hero image"
+              className="w-full object-cover"
+              width="500"
+              height="700"
+              loading="lazy"
+              decoding="async"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="sm:flex-1 static sm:relative">
+        <div className="py-20 px-10 sm:p-32">
+          <h4 className="text-3xl highlight w-fit font-jp mb-8 py-1">よ。</h4>
+          <p>Hi. Thanks for stopping by.</p>
+          <p>I'm Eiji, a Japanese living in Tokyo.</p>
+          <p>I write code for a living.</p>
+          <p>I love traveling.</p>
+          <p>I have short legs and enlarged pores.</p>
+          <p>I live quite a low-key life.</p>
+          <p>
+            I'm usually not active when it comes to online conversations because
+            I want to save the excitement till I meet that person in real life.
+            Sometimes it turns out well, but most of the time people see me as a
+            boring person :)
+          </p>
+          <br />
+          <p className="text-sm">
+            2025. Made with{" "}
+            <a href="https://nextjs.org/" target="_blank" className="highlight">
+              Next.js
+            </a>
+            .
+          </p>
+          <div className="flex gap-2 mt-8">
+            <a
+              href="https://github.com/hellomyzn/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithub className="w-6 h-6 cursor-pointer" />
+            </a>
+            <a
+              href="https://www.instagram.com/hellomyzn/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagram className="w-6 h-6 cursor-pointer" />
+            </a>
+            <a
+              href="https://www.facebook.com/hellomyzn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebook className="w-6 h-6 cursor-pointer" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hellomyzn/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin className="w-6 h-6 cursor-pointer" />
+            </a>
+            <a
+              href="https://x.com/hellohanaki"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitter className="w-6 h-6 cursor-pointer" />
+            </a>
+            <a
+              href="https://qiita.com/hellomyzn"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SiQiita className="w-6 h-6 cursor-pointer" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
