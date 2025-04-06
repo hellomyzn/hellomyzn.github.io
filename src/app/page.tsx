44c932503bf06b2bcc29aa2619f2b147f08544a1
hellomyzn <em711th@icloud.com>
2025-04-06 16:53:15 +0900

    update: home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,107 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full flex h-screen flex-col sm:flex-row">
+      {/* <!-- first column --> */}
+      <div className="flex flex-col w-full sm:w-1/2">
+        {/* <!-- myzn --> */}
+        <div className="h-screen sm:h-64 sm:border-r flex justify-center items-center sm:justify-end sm:items-end flex-col pb-7 sm:pr-10">
+          <div className="relative">
+            <h1 className="text-5xl">
+              <span className="tracking-tight text-[120px]">myzn</span>
+            </h1>
+            <p className="">__A slothful creature from Earth__</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        {/* <!-- Under myzn --> */}
+        <div className="hidden sm:grid grid-rows-[100px_1fr_1fr_1fr] grid-cols-[1fr_1fr_1fr_80px] flex-1">
+          <div className="border-r border-t col-span-4 flex justify-center items-center ">
+            <p className="text-2xl uppercase">We know very little</p>
+          </div>
+          <div className="border-r border-t row-start-2 row-span-3 col-start-1 col-span-1 "></div>
+          {/* <!-- IMAGE --> */}
+          <div className="border-r border-t row-start-2 row-span-3 col-start-2 col-span-2 px-8">
+            <img
+              src="/images/me.jpg"
+              className="mt-8 mb-4 -ml-24"
+              alt="sample"
+              width={1000}
+              height={500}
+              loading="lazy"
+              decoding="async"
+            />
+            <div className="">
+              <p className="text-2xl highlight w-fit mb-2 py-1 px-0.5">い am</p>
+              <p className="text-sm">
+                __an extroverted introvert, if that makes sense__
+              </p>
+            </div>
+          </div>
+          <div className="border-r row-start-2 row-span-3 col-start-4 col-span-1 flex justify-center items-center">
+            <p className="uppercase rotate-90 whitespace-nowrap">
+              I'm too おld to care
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <!-- FOR SM --> */}
+      <div className="sm:hidden">
+        <div className="border-t w-full py-8 flex items-center justify-center">
+          <p className="text-xl uppercase">We know very little</p>
+        </div>
+        <div className="p-8 border-t">
+          <p className="text-xl font-jp highlight w-fit mb-2">い am</p>
+          <p>__an extroverted introvert, if that makes sense__</p>
+        </div>
+        <div className="border-t w-full py-8 flex items-center justify-center">
+          <p className="text-xl uppercase">I'm too おld to care</p>
+        </div>
+      </div>
+
+      {/* <!-- right column --> */}
+      <div className="w-full sm:w-1/2 sm:grid grid-rows-[100px_1fr_1fr] grid-cols-2">
+        {/* <!-- Nav --> */}
+        <div className="hidden col-span-2 row-span-1 sm:flex justify-end items-start"></div>
+        {/* <!-- Under nav HERO image --> */}
+        <div className="sm:border-r sm:border-t col-start-1 col-span-1 row-span-2 flex items-center overflow-hidden">
+          <img
+            src="/images/top.jpg"
+            className="h-full object-cover"
+            alt="hero image"
+            width="800"
+            height="1200"
+            loading="lazy"
+            decoding="async"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+        <div className="border-t py-10 flex text-right flex-col sm:hidden pr-8">
+          <p className="text-3xl font-jp">
+            <span className="tracking-tighter">え</span>
+            njoying
+          </p>
+          <p className="text-sm">
+            <span>__being average__</span>
+          </p>
+        </div>
+        <div className="border-t sm:border-b col-start-2 col-span-1 row-span-1">
+          <div className="p-6 sm:px-8 flex-row sm:flex-col sm:py-10 m-auto sm:mt-8 sm:p-0 w-fit text-center h-full">
+            <p className="uppercase highlight mb-3 sm:mb-2 text-3xl">
+              I'm not lazy
+            </p>
+            <p className="text-sm">__I'm on energy saving mode__</p>
+          </div>
+        </div>
+        <div className="hidden col-start-2 col-span-1 row-span-1 sm:flex items-center">
+          <div className="ml-4">
+            <p className="text-3xl font-jp">
+              <span className="tracking-tighter">え</span>
+              njoying
+            </p>
+            <p className="text-sm">
+              <span>__being average__</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
