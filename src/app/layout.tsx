1aebf8d77c5bcf7a72ae9d7e8bacb887cb09f5a4
hellomyzn <em711th@icloud.com>
2025-04-09 18:12:13 +0900

    update: google analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from "./Header"; // ヘッダーコンポーネントをインポート
 import "./globals.css";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body className="relative min-h-screen overflow-x-hidden font-thin">
         <Header />
 
